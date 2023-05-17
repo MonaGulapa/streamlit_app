ada7e45a0488cd6539faa1404c46285f3b3f6aa8
MonaGulapa <39524468+MonaGulapa@users.noreply.github.com>
2023-05-17 19:56:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,19 +45,27 @@ except URLError as e:
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response.json()) # just writes data to the screen
 
+
+streamlit.header("The Fruit Load List Contains:")
+# Snowflake related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+  
+# add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text(my_data_row)
+
 # don't run anything past here while we troubleshoot
 streamlit.stop() 
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
-# my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-# streamlit.text("Hello from Snowflkae:")
-streamlit.header("The Fruit Load List Contains:")
-# streamlit.text(my_data_row)
 streamlit.dataframe(my_data_rows)
 add_fruit = streamlit.text_input('What fruit would you like to add?',"")
 streamlit.write('Thanks for adding', add_fruit)
